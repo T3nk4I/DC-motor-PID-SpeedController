55c55447cb7e6adade4defefbc6ce3a7499913af
T3nk4I <finnandjake1010@gmail.com>
2025-12-18 09:00:22 -0600

    updated with new control

--- src/main.cpp
+++ src/main.cpp
@@ -1,144 +1,157 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <AS5600.h>
-#include <Arduino.h>
 #include <Wire.h>
-#include <util/atomic.h>
-#include <util/delay.h>
 
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+// --- Configuration ---
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-#define R_Enable PD2
-#define L_Enable PD3
-#define R_PWM PB1
-#define L_PWM PB2
-#define R_IS PC0
-#define L_IS PC1
+#define R_Enable 2
+#define L_Enable 3
+#define R_PWM 10
+#define L_PWM 9
 
 #define PREV PD6
 #define SELECT PD7
-#define NEXT PB0
 
+// --- PI Constants (Tune these!) ---
+float Kp = 0.3;   // Proportional gain
+float Ki = 1.5;   // Integral gain
+
+// --- Variables ---
+float targetRPM = 0;
+float currentRPM = 0;
+float integral = 0;
+float outputPWM = 0;
+unsigned long lastTime = 0;
+
+unsigned long lastDisplayUpdate = 0;
+const unsigned long displayInterval = 300; // Update OLED every 300ms
+
+bool lastPrevState = HIGH;
+bool lastSelectState = HIGH;
+
+// --- Classes ---
 class Motor {
    public:
     void init() {
-        DDRD |= (1 << R_Enable) | (1 << L_Enable);
-        DDRB |= (1 << R_PWM) | (1 << L_PWM);
+        pinMode(R_Enable, OUTPUT);
+        pinMode(L_Enable, OUTPUT);
+        pinMode(R_PWM, OUTPUT);
+        pinMode(L_PWM, OUTPUT);
+        digitalWrite(R_Enable, HIGH); // Enable the driver
+        digitalWrite(L_Enable, HIGH);
     }
 
-    void move(byte A, char dir) {
-        PORTD |= (1 << R_Enable) | (1 << L_Enable);
-        if (dir == 'F') {
-            analogWrite(R_PWM, A);
-            PORTD &= ~(1 << L_PWM);
-
-        } else if (dir == 'B') {
-            analogWrite(L_PWM, A);
-            PORTD &= ~(1 << R_PWM);
+    void drive(float pwmValue) {
+        // Logic: PWM > 0 Forward, PWM < 0 Backward
+        if (pwmValue >= 0) {
+            int val = (int)constrain(pwmValue, 0, 255);
+            analogWrite(R_PWM, val);
+            analogWrite(L_PWM, 0);
+        } else {
+            int val = (int)constrain(abs(pwmValue), 0, 255);
+            analogWrite(L_PWM, val);
+            analogWrite(R_PWM, 0);
         }
     }
 };
-
-// Global Variables
-float Kp = 0.8;
-float Ki = 1.2;
-
-float targetRPM = 0, currentRPM = 0;
-float err = 0, lastError = 0;
-float integral = 0;
-float output = 0;
-
-unsigned long lastTime = 0;
-
-int counter = 0;
-int PWM;
-
-char direction;
-
-float current;
-int getCurrent(int pin) {
-    return analogRead(pin) / 1024 * 8.5;
-}
 
 Motor BTS7960;
 AS5600 encoder;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Wire.begin();
-
     BTS7960.init();
 
-    pinMode(R_Enable, OUTPUT);
-    pinMode(L_Enable, OUTPUT);
-    pinMode(R_PWM, OUTPUT);
-    pinMode(L_PWM, OUTPUT);
+    pinMode(PREV, INPUT_PULLUP);
+    pinMode(SELECT, INPUT_PULLUP);
 
-    DDRD &= ~(1 << PREV) | (1 << SELECT);
-    DDRB &= ~(1 << NEXT);
-
-    Serial.begin(9600);
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-        Serial.println(F("SSD1306 allocation failed"));
-        while (true);
-    }
-    if (!encoder.begin()) {
-        Serial.println(F("AS5600 allocation failed"));
-        while (true);
-    }
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) while (true);
+    encoder.begin();
     
     display.clearDisplay();
-    display.setCursor(0, 0);
     display.setTextColor(WHITE);
-    display.println("starting...");
+    lastTime = millis();
 }
 
-void oledWrite(String msg) {
+void readButtons() {
+    bool currentPrev = digitalRead(PREV);
+    bool currentSelect = digitalRead(SELECT);
+
+    if (currentPrev == LOW && lastPrevState == HIGH) {
+        targetRPM -= 50; // Decrease target speed
+    }
+    lastPrevState = currentPrev;
+
+    if (currentSelect == LOW && lastSelectState == HIGH) {
+        targetRPM += 50; // Increase target speed
+    }
+    lastSelectState = currentSelect;
+
+    targetRPM = constrain(targetRPM, 0, 2000); // Set your motor's max RPM
+}
+
+void updateOLED() {
     display.clearDisplay();
+    display.setTextSize(1);
     display.setCursor(0, 0);
-    display.setTextSize(1.5);
-    display.println("Seleccione RPM");
-    display.setTextSize(3);
+    display.print("Target RPM: ");
+    display.println((int)targetRPM);
+
     display.setCursor(0, 20);
-    display.println(msg);
+    display.print("Actual RPM:");
+    display.setTextSize(2);
+    display.setCursor(0, 35);
+    display.print((int)currentRPM);
+
+    display.setTextSize(1);
+    display.setCursor(80, 50);
+    display.print("PWM:");
+    display.print((int)outputPWM);
+    
     display.display();
 }
 
 void loop() {
-    if (!(PIND & (1 << PREV))) {
-        _delay_ms(10);
-        counter--;
-    }
-    if (!(PIND & (1 << SELECT))) {
-        _delay_ms(10);
-        counter++;
-    }
+    // 1. Read Target (Buttons)
+    readButtons();
 
-    Serial.println(counter);
-    oledWrite(String(counter));
-    if (counter > 0) {
-        direction = 'F';
-    } else {
-        direction = 'B';
-    }
-
+    // 2. Compute PI every loop iteration
     unsigned long currentTime = millis();
-    float dt = (currentTime - lastTime) / 1000.0;
+    float dt = (currentTime - lastTime) / 1000.0; // Delta time in seconds
     
-    targetRPM = abs(counter);
-    currentRPM = encoder.getAngularSpeed(AS5600_MODE_RPM);
+    if (dt > 0) {
+        // Get actual speed
+        if (encoder.detectMagnet()) {
+            currentRPM = encoder.getAngularSpeed(AS5600_MODE_RPM);
+        } else {
+            currentRPM = 0;
+        }
 
-    err = targetRPM - currentRPM;
-    if (abs(integral) < 255) {
-        integral += err * dt;
+        // Calculate Error
+        float error = targetRPM - currentRPM;
+
+        // Calculate Integral with Anti-Windup
+        integral += error * dt;
+        integral = constrain(integral, -150, 150); // Prevent "runaway" power
+
+        // Calculate PI Output
+        outputPWM = (Kp * error) + (Ki * integral);
+        
+        // Command Motor
+        BTS7960.drive(outputPWM);
+
+        lastTime = currentTime;
     }
 
-    output = Kp * err + Ki * integral;
-
-    BTS7960.move(output, direction);
-
-    lastError = err;
-    lastTime = currentTime;
+    // 3. Update Display (Non-blocking)
+    if (millis() - lastDisplayUpdate >= displayInterval) {
+        updateOLED();
+        lastDisplayUpdate = millis();
+    }
 }
+
