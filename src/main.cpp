b6e9049c732f57939f87e53bfc17893fd6599742
T3nk4I <finnandjake1010@gmail.com>
2025-12-18 05:46:32 -0600

    updates code, now uses button inputs instead of rotary encoder, changed PWM pins for motor control

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <util/atomic.h>
+#include <util/delay.h>
 
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
@@ -10,14 +11,14 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 #define R_Enable PD2
 #define L_Enable PD3
-#define R_PWM PD5
-#define L_PWM PD6
+#define R_PWM PB1
+#define L_PWM PB2
 #define R_IS PC0
 #define L_IS PC1
 
-#define BTN PB0
-#define CLK PD7
-#define DT PD4
+#define PREV PD6
+#define SELECT PD7
+#define NEXT PB0
 
 class Motor {
    public:
@@ -26,19 +27,17 @@ class Motor {
     }
 
     void move(byte A, char dir) {
+        PORTD |= (1 << R_Enable) | (1 << L_Enable);
         if (dir == 'F') {
             analogWrite(R_PWM, A);
-            PORTD |= (1 << R_Enable);
-            PORTD &= ~(1 << L_Enable);
+            PORTD &= ~(1 << L_PWM);
+
         } else if (dir == 'B') {
             analogWrite(L_PWM, A);
-            PORTD |= (1 << L_Enable);
-            PORTD &= ~(1 << R_Enable);
+            PORTD &= ~(1 << R_PWM);
         }
     }
 };
-
-Motor BTS7960;
 
 // Global Variables
 int Kp;
@@ -50,18 +49,25 @@ float err;
 float current;
 
 int counter = 0;
-int direction = 0;
-int CLK_state, prev_CLK_state;
+char direction;
 
 int getCurrent(int pin) {
     return analogRead(pin) / 1024 * 8.5;
 }
 
+Motor BTS7960;
+
 void setup() {
     Serial.begin(9600);
     BTS7960.init();
-    DDRD &= ~(1 << CLK) | (1 << DT);
-    DDRB &= ~(1 << BTN);
+
+    pinMode(R_Enable, OUTPUT);
+    pinMode(L_Enable, OUTPUT);
+    pinMode(R_PWM, OUTPUT);
+    pinMode(L_PWM, OUTPUT);
+
+    DDRD &= ~(1 << PREV) | (1 << SELECT);
+    DDRB &= ~(1 << NEXT);
 
     Serial.begin(9600);
     if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
@@ -71,7 +77,6 @@ void setup() {
     display.clearDisplay();
     display.setCursor(0, 0);
     display.setTextColor(WHITE);
-    prev_CLK_state = (PIND & (1 << CLK)) >> CLK;
     display.println("starting...");
 }
 
@@ -87,17 +92,22 @@ void oledWrite(String msg) {
 }
 
 void loop() {
-    CLK_state = (PIND & (1 << CLK)) >> CLK;
-    if (CLK_state != prev_CLK_state) {  // step has occurred
-        if (((PIND & (1 << DT)) >> DT) != CLK_state) {
-            counter--;
-            direction = 1;
-        } else {
-            counter++;
-            direction = 0;
-        }
+    if (!(PIND & (1 << PREV))) {
+        _delay_ms(10);
+        counter--;
     }
+    if (!(PIND & (1 << SELECT))) {
+        _delay_ms(10);
+        counter++;
+    }
+
     Serial.println(counter);
     oledWrite(String(counter));
-    prev_CLK_state = CLK_state;
+    if (counter > 0) {
+        direction = 'F';
+    } else {
+        direction = 'B';
+    }
+
+    BTS7960.move(abs(counter), direction);
 }
